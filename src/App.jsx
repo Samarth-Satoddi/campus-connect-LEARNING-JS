d7ace691a6de83d6dbf074790c473de65a098c5b
Sandeep G N <sandeepgn916@gmail.com>
2026-09-22 10:11:25 +0530

    Add JavaScript learning code

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import AboutPage from "./pages/AboutPage";
+
+import { initialEvents } from "./data/events";
+
+function App() {
+    const [events, setEvents] = useState(initialEvents);
+
+    function handleAddEvent(newEvent) {
+        setEvents([...events, newEvent]);
+    }
+
+    function handleDeleteEvent(eventId) {
+        const updatedEvents = events.filter(function (event) {
+            return event.id !== eventId;
+        });
+
+        setEvents(updatedEvents);
+    }
+
+    return (
+        <div>
+            <Navbar />
+
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <HomePage
+                            events={events}
+                            onAddEvent={handleAddEvent}
+                            onDeleteEvent={handleDeleteEvent}
+                        />
+                    }
+                />
+
+                <Route
+                    path="/events"
+                    element={
+                        <EventsPage
+                            events={events}
+                            onDeleteEvent={handleDeleteEvent}
+                        />
+                    }
+                />
+
+                <Route
+                    path="/events/:eventId"
+                    element={
+                        <EventDetailsPage
+                            events={events}
+                        />
+                    }
+                />
+
+                <Route
+                    path="/about"
+                    element={<AboutPage />}
+                />
+            </Routes>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
